4503291c0413107824d706de1be80bf9afe9082a
David Korth <gerbilsoft@gerbilsoft.com>
2020-08-13 22:59:23 -0400

    [libortin] ndscrypt: Use a context for encryption.

--- src/libortin/ndscrypt.cpp
+++ src/libortin/ndscrypt.cpp
@@ -285,15 +285,57 @@ static const uint8_t encr_data[] = {
 };
 
 // Encryption context.
-// TODO: Make it an actual context later.
-static uint32_t card_hash[0x412];
-static int cardheader_devicetype = 0;		// TODO: DSi?
-static uint32_t global3_x00, global3_x04;	// RTC value
-static uint32_t global3_rand1;
-static uint32_t global3_rand3;
-static uint32_t arg2[3];
+class NDSCrypt
+{
+	public:
+		NDSCrypt(uint32_t gamecode);
 
-static uint32_t lookup(uint32_t *magic, uint32_t v)
+	private:
+		static uint32_t lookup(uint32_t *magic, uint32_t v);
+
+		static void encrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2);
+		static void decrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2);
+
+		static void encrypt(uint32_t *magic, uint64_t &cmd);
+		static void decrypt(uint32_t *magic, uint64_t &cmd);
+
+		static void update_hashtable(uint32_t* magic, uint8_t arg1[8]);
+		static void init2(uint32_t *magic, uint32_t a[3]);
+		void init1(void);
+
+	public:
+		void init0(void);
+		int decrypt_arm9(uint8_t *data);
+		int encrypt_arm9(uint8_t *data);
+
+		inline const uint32_t *card_hash(void) const
+		{
+			return m_card_hash;
+		}
+
+	private:
+		uint32_t m_gamecode;
+
+		uint32_t m_card_hash[0x412];
+		int m_cardheader_devicetype;		// TODO: DSi?
+		uint32_t m_global3_x00, m_global3_x04;	// RTC value
+		uint32_t m_global3_rand1;
+		uint32_t m_global3_rand3;
+		uint32_t m_arg2[3];
+};
+
+NDSCrypt::NDSCrypt(uint32_t gamecode)
+	: m_gamecode(gamecode)
+	, m_cardheader_devicetype(0)
+	, m_global3_x00(0)
+	, m_global3_x04(0)
+	, m_global3_rand1(0)
+	, m_global3_rand3(0)
+{
+	memset(m_arg2, 0, sizeof(m_arg2));
+}
+
+uint32_t NDSCrypt::lookup(uint32_t *magic, uint32_t v)
 {
 	uint32_t a = (v >> 24) & 0xFF;
 	uint32_t b = (v >> 16) & 0xFF;
@@ -308,7 +350,7 @@ static uint32_t lookup(uint32_t *magic, uint32_t v)
 	return d + (c ^ (b + a));
 }
 
-static void encrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
+void NDSCrypt::encrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
 {
 	uint32_t a,b,c;
 	a = *arg1;
@@ -323,7 +365,7 @@ static void encrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
 	*arg1 = b ^ magic[17];
 }
 
-static void decrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
+void NDSCrypt::decrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
 {
 	uint32_t a,b,c;
 	a = *arg1;
@@ -338,17 +380,17 @@ static void decrypt(uint32_t *magic, uint32_t *arg1, uint32_t *arg2)
 	*arg2 = a ^ magic[1];
 }
 
-static void encrypt(uint32_t *magic, uint64_t &cmd)
+void NDSCrypt::encrypt(uint32_t *magic, uint64_t &cmd)
 {
 	encrypt(magic, (uint32_t *)&cmd + 1, (uint32_t *)&cmd + 0);
 }
 
-static void decrypt(uint32_t *magic, uint64_t &cmd)
+void NDSCrypt::decrypt(uint32_t *magic, uint64_t &cmd)
 {
 	decrypt(magic, (uint32_t *)&cmd + 1, (uint32_t *)&cmd + 0);
 }
 
-static void update_hashtable(uint32_t* magic, uint8_t arg1[8])
+void NDSCrypt::update_hashtable(uint32_t* magic, uint8_t arg1[8])
 {
 	for (int j=0;j<18;j++)
 	{
@@ -377,30 +419,30 @@ static void update_hashtable(uint32_t* magic, uint8_t arg1[8])
 	}
 }
 
-static void init2(uint32_t *magic, uint32_t a[3])
+void NDSCrypt::init2(uint32_t *magic, uint32_t a[3])
 {
 	encrypt(magic, a+2, a+1);
 	encrypt(magic, a+1, a+0);
 	update_hashtable(magic, (uint8_t*)a);
 }
 
-static void init1(uint32_t cardheader_gamecode)
+void NDSCrypt::init1(void)
 {
-	memcpy(card_hash, &encr_data, 4*(1024 + 18));
-	arg2[0] = *(uint32_t *)&cardheader_gamecode;
-	arg2[1] = (*(uint32_t *)&cardheader_gamecode) >> 1;
-	arg2[2] = (*(uint32_t *)&cardheader_gamecode) << 1;
-	init2(card_hash, arg2);
-	init2(card_hash, arg2);
+	memcpy(m_card_hash, &encr_data, 4*(1024 + 18));
+	m_arg2[0] = m_gamecode;
+	m_arg2[1] = m_gamecode >> 1;
+	m_arg2[2] = m_gamecode << 1;
+	init2(m_card_hash, m_arg2);
+	init2(m_card_hash, m_arg2);
 }
 
-static void init0(uint32_t cardheader_gamecode)
+void NDSCrypt::init0(void)
 {
-	init1(cardheader_gamecode);
-	encrypt(card_hash, (uint32_t*)&global3_x04, (uint32_t*)&global3_x00);
-	global3_rand1 = global3_x00 ^ global3_x04;		// more RTC
-	global3_rand3 = global3_x04 ^ 0x0380FEB2;
-	encrypt(card_hash, (uint32_t*)&global3_rand3, (uint32_t*)&global3_rand1);
+	init1();
+	encrypt(m_card_hash, (uint32_t*)&m_global3_x04, (uint32_t*)&m_global3_x00);
+	m_global3_rand1 = m_global3_x00 ^ m_global3_x04;		// more RTC
+	m_global3_rand3 = m_global3_x04 ^ 0x0380FEB2;
+	encrypt(m_card_hash, (uint32_t*)&m_global3_rand3, (uint32_t*)&m_global3_rand1);
 }
 
 // ARM9 decryption check values
@@ -410,16 +452,16 @@ static void init0(uint32_t cardheader_gamecode)
 /*
  * decrypt_arm9
  */
-static int decrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
+int NDSCrypt::decrypt_arm9(uint8_t *data)
 {
 	uint32_t *p = (uint32_t*)data;
 
-	init1(cardheader_gamecode);
-	decrypt(card_hash, p+1, p);
-	arg2[1] <<= 1;
-	arg2[2] >>= 1;	
-	init2(card_hash, arg2);
-	decrypt(card_hash, p+1, p);
+	init1();
+	decrypt(m_card_hash, p+1, p);
+	m_arg2[1] <<= 1;
+	m_arg2[2] >>= 1;	
+	init2(m_card_hash, m_arg2);
+	decrypt(m_card_hash, p+1, p);
 
 	if (p[0] != MAGIC30 || p[1] != MAGIC34)
 	{
@@ -431,7 +473,7 @@ static int decrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
 	uint32_t size = 0x800 - 8;
 	while (size > 0)
 	{
-		decrypt(card_hash, p+1, p);
+		decrypt(m_card_hash, p+1, p);
 		p += 2;
 		size -= 8;
 	}
@@ -442,7 +484,7 @@ static int decrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
 /*
  * encrypt_arm9
  */
-static int encrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
+int NDSCrypt::encrypt_arm9(uint8_t *data)
 {
 	uint32_t *p = (uint32_t*)data;
 	if (p[0] != cpu_to_le32(0xE7FFDEFF) || p[1] != cpu_to_le32(0xE7FFDEFF))
@@ -451,17 +493,17 @@ static int encrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
 	}
 	p += 2;
 
-	init1(cardheader_gamecode);
+	init1();
 
-	arg2[1] <<= 1;
-	arg2[2] >>= 1;
+	m_arg2[1] <<= 1;
+	m_arg2[2] >>= 1;
 	
-	init2(card_hash, arg2);
+	init2(m_card_hash, m_arg2);
 
 	uint32_t size = 0x800 - 8;
 	while (size > 0)
 	{
-		encrypt(card_hash, p+1, p);
+		encrypt(m_card_hash, p+1, p);
 		p += 2;
 		size -= 8;
 	}
@@ -470,9 +512,9 @@ static int encrypt_arm9(uint32_t cardheader_gamecode, unsigned char *data)
 	p = (uint32_t*)data;
 	p[0] = MAGIC30;
 	p[1] = MAGIC34;
-	encrypt(card_hash, p+1, p);
-	init1(cardheader_gamecode);
-	encrypt(card_hash, p+1, p);
+	encrypt(m_card_hash, p+1, p);
+	init1();
+	encrypt(m_card_hash, p+1, p);
 	return 0;
 }
 
@@ -494,7 +536,8 @@ int encryptSecureArea(uint8_t *pRom)
 	}
 
 	uint32_t gamecode = le32_to_cpu(pRom32[0x0C/4]);
-	encrypt_arm9(gamecode, &pRom[0x4000]);
+	NDSCrypt ndsCrypt(gamecode);
+	ndsCrypt.encrypt_arm9(&pRom[0x4000]);
 
 	// Calculate CRCs.
 	uint16_t *pRom16 = reinterpret_cast<uint16_t*>(pRom);
@@ -502,16 +545,17 @@ int encryptSecureArea(uint8_t *pRom)
 	pRom16[0x6C/2] = cpu_to_le16(CalcCrc16(&pRom[0x4000], 0x4000));
 	// Header CRC16
 	pRom16[0x15E/2] = cpu_to_le16(CalcCrc16(pRom, 0x15E));
-	
-	init0(gamecode);
-	srand(gamecode);
+
+	// Reinitialize the card hash.
+	ndsCrypt.init0();
+	//srand(gamecode);	// FIXME: Is this actually needed?
 	
 	// rounds table
-	memcpy(&pRom[rounds_offsets], card_hash, 4*18);
+	memcpy(&pRom[rounds_offsets], ndsCrypt.card_hash(), 4*18);
 	
 	// S-boxes
 	for (unsigned int i = 0; i < 4; i++) {
-		memcpy(&pRom[sbox_offsets + (4*256*i)], &card_hash[18 + (i*256)], 4*256);
+		memcpy(&pRom[sbox_offsets + (4*256*i)], &ndsCrypt.card_hash()[18 + (i*256)], 4*256);
 	}
 
 	// test patterns
